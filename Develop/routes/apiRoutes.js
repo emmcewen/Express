0b6fb5354d34c2bd9453301d23c7621028830dae
EMILY MCEWEN <emmcewen@gmail.com>
2022-07-27 23:10:33 -0400

    adding delete for bonus

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,42 +1,48 @@
-const router=require("express").Router()
+const router = require("express").Router()
+const fs = require("fs")
 
+router.get("/api/notes/:id", (req, res) => {
+    res.json(notes[req.params.id]);
+})
 
-router.get("/api/notes",(req,res)=>{
-    const fs=require("fs")
-    const db=require("../db/db.json");
-    res.json(db)
+router.get("/api/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        res.json(notes);
+    })
 })
 
 
-router.post("/api/notes", (req,res) => {
-   fs.readFile("../db/db.json", "utf8", (err,data)=>{
-       if (err) throw err;
-       var notes =JSON.parse(data);
-       let userNote =req.body;
-       userNote.id = Math.floor(Math.random()*5000);
-       notes.push(userNote);
-       fs.writeFile("../db/db.json",JSON.stringify(notes),(err,data) => {
-           res.json(userNote);
-           console.log(req.body);
-    
-   });
+router.post("/api/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        let userNote = req.body;
+        userNote.id = Math.floor(Math.random() * 5000);
+        notes.push(userNote);
+        fs.writeFile("../db/db.json", JSON.stringify(notes), (err, data) => {
+            res.json(userNote);
+
+        });
+    });
 
 });
-    
-    
-/*router.delete("/api/notes:id", (req,res) => {
-    fs.readFile("../db/db.json", "utf8", (err,data)=>{
-      if (err) throw err;
-       let notes =JSON.parse(data);
-        const newNotes=notes.filter(note=> note.id !==parseInt(req.params.id));
 
-        fs.writeFile(("../db/db.json",JSON.stringify(newNotes),(err,data) => {
-            res.json({message:"success"});
+router.delete("/api/notes:id", (req, res) => {
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
 
-        }    
-));*/  
+        fs.writeFile(("../db/db.json", JSON.stringify(newNotes), (err, data) => {
+            res.json({ message: "success" });
 
-})
+        }
+        ));
+
+    });
+});
 
 
-module.exports=router
+    module.exports = router
