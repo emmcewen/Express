6022524555c0626e545695518c4ce7ef66756d7e
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-05 11:57:20 -0400

    adding updated code to work on bugs

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,15 @@
-const express =require("express")
-const htmlRoute=require("./routes/htmlRoutes")
-const apiRoute=require("./routes/apiRoutes")
-const PORT= process.env.PORT || 3001
+const express =require("express");
+const fs =require('fs');
+const path =require ('path');
+const htmlRoutes=require("./routes/htmlRoutes");
+const apiRoutes=require("./routes/apiRoutes");
+const PORT= process.env.PORT || 3001;
 
-const app = express()
-app.use(express.static("public"))
+const app = express();
+app.use(express.static("public"));
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use(apiRoutes)
 
